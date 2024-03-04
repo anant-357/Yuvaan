f93fb4ab99b3076008e19a425c52922692cd8a6d
anant-357 <anantsh.88@gmail.com>
2024-03-04 09:44:17 +0530

    changed basename

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/Yuvaan/",
 
   plugins: [react()],
   css: {
@@ -14,3 +14,4 @@ export default defineConfig({
     },
   },
 });
+
